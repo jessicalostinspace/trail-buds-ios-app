d99361e0b0a300552bfb7faba44a8cf8a00750a3
Jessica Wilson <jessicalostinspace@gmail.com>
2016-04-01 13:33:41 -0700

    profile page storyboard changes made

--- TrailBuds/ProfileViewController.swift
+++ TrailBuds/ProfileViewController.swift
@@ -187,7 +187,7 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("allEventsCell", forIndexPath: indexPath) as! allEventsCell
+        let cell = upcomingHikesTableView.dequeueReusableCellWithIdentifier("upcomingHikesCell", forIndexPath: indexPath) as! UpcomingHikesTableViewCell
         
         return cell
     }
